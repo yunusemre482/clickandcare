8b1a02fdc93144e44a089b7f4722638176494464
Yunus emre akgün <akgunyunus482@gmail.com>
2024-12-25 22:55:05 +0300

    feat: initialize iOS project structure with necessary configuration and launch assets

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.27.1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yunusemre/Desktop/Personal/clickandcare"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/yunusemre/Desktop/Personal/clickandcare/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/yunusemre/Desktop/Personal/clickandcare/.dart_tool/package_config.json"
